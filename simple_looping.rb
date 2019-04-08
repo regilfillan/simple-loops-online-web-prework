03c86b44bf8ee4c9522a1bd065d7ba69d37820c1
Ryan Gilfillan <regilfillan@gmail.com>
2019-04-08 03:05:26 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -26,7 +26,7 @@ def until_iterator(number_of_times)
   counter = 1
   until counter > number_of_times
   puts "Welcome to Flatiron School's Web Development Course!"
-  coutner += 1
+  counter += 1
 end
 end
 
